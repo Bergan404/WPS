7ed0da35df5a52b79a4f8fbf022e5f7d027282ac
Bergan Oudshoorn <Bergan404@gmail.com>
2024-12-02 16:05:03 -0500

    finalized

---
+++ src/assets/scripts/nav.js
@@ -0,0 +1,40 @@
+// Burger Nav
+
+$("#burger-nav").click(() => {
+  $("#burger-nav").toggleClass("open");
+  $(".mobile-nav").toggleClass("open");
+  $(".burger-overlay").toggleClass("show");
+});
+
+$(".collapse-bar").click(() => {
+  $(".shape-involved").toggleClass("open");
+  $(".collapse-bar").toggleClass("close");
+});
+
+// Banner
+
+$("#close-banner").click(() => {
+  $("#banner").hide();
+});
+
+// Progress Bar
+
+const progressWrap = document.getElementById("progress-wrap");
+
+if (progressWrap) {
+  const replaceComma = (num) => {
+    return num.replace(/,/g, "");
+  };
+  let percentage =
+    (replaceComma($("#current-count").html()) /
+      replaceComma($("#goal").html())) *
+    100;
+  $($("#progress")).css("width", percentage + "%");
+}
+
+/* Burger nav */
+$(".burger, .burger-overlay").click(function () {
+  $(".#burger-nav").toggleClass("clicked");
+  $(".mobile-nav").toggleClass("show");
+  $("body").toggleClass("overflow");
+});
